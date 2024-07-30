192317992eb570512d9a2a85930f02712612ada1
SantiagoJS10 <david1011jimenez@gmail.com>
2024-07-29 23:31:55 -0500

    creacion del login

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Login from './Login'
+
+function App() {
+  
+
+  return (
+    <div className='text-white h-[100vh] flex items-center  justify-center bg-cover ' style={{backgroundImage: "url('./src/assets/img/fondo.avif')"}}>
+    <Login/>
+
+    </div>
+  )
+}
+
+export default App
